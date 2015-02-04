6aa715a6fd415e7154684fd001fc670940c53072
何翔华 <hxh@ike-global.com>
2015-02-04 09:03:00 +0800

    更新

--- CAF.Web/Scripts/app/app.js
+++ CAF.Web/Scripts/app/app.js
@@ -1,20 +1,21 @@
 ﻿var appManage = angular.module('RouteApp', ['ngRoute', 'restangular']);
 
 
-appManage.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-
-    $locationProvider.hashPrefix('!');
-    $routeProvider.when("/organizes", {
-            controller: "organizeController",
-            templateUrl: "/Manage/Organizes"
-        })
-        .when("/index", {
-            controller: "indexController",
-            templateUrl: "/Manage/Index"
-        })
-        .when("/users", {
-            controller: "userController",
-            templateUrl: "/Manage/Users"
-        })
-        .otherwise({ redirectTo: '/' });
-}]);
+appManage.config([
+        '$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+            $locationProvider.html5Mode(false).hashPrefix('!');
+            $routeProvider.when("/organizes", {
+                    controller: "organizeController",
+                    templateUrl: "/Manage/Organizes"
+                })
+                .when("/index", {
+                    controller: "indexController",
+                    templateUrl: "/Manage/Index"
+                })
+                .when("/users", {
+                    controller: "userController",
+                    templateUrl: "/Manage/Users"
+                })
+                .otherwise({ redorectTo: "/" });
+        }
+    ]);
