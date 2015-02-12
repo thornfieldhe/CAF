3ece9651d8884866012354029c13509394aa0111
何翔华 <hxh@ike-global.com>
2015-02-12 23:00:54 +0800

    设计部门编辑标签

--- CAF.Web/Scripts/app/services/organizeService.js
+++ CAF.Web/Scripts/app/services/organizeService.js
@@ -8,7 +8,7 @@ appManage.factory('organizeService', ['Restangular', function (Restangular) {
     return {
         getOrganizes: function () { return _orgnizeService.getList(); },
         newOrganize: function () {
-            return {Title:"新增组织结构",Level:"01",Parent:"集团公司"}
+            return {Title:"新增组织结构",Level:"01",Parent:"集团公司",Sort:1000}
         }
     }
 }]);
